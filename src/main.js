74142839aeff1adafa119aa60101c4128e367b19
Axent13 <Axent13@yandex.ru>
2016-11-13 17:43:13 +0700

    Динамическая загрузка стилей

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './fonts/fonts.styl';
 import './index.styl';
-import './components/header/header.styl';
-import './components/phone/phone.styl';
-import './components/info/info.styl';
-import './components/lets-talk/lets-talk.styl'
-import './components/footer/footer.styl';
+
+require.context("./components", true, /.styl$/);
